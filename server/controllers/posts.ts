ff04d8996cb98e7e5979423ce366f5aa96504cf9
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-19 12:12:48 +0530

    (feat): add model controls

--- server/controllers/posts.ts
+++ server/controllers/posts.ts
@@ -1,4 +1,27 @@
-export const getPosts = (req: any, res: any) => {
-  res.send("Working");
-  console.log(req);
+import PostMessage from "../models/postMessage";
+
+export const getPosts = async (req: any, res: any) => {
+  const body = req.body;
+
+  try {
+    const postMessages = await PostMessage.find();
+
+    console.log(body);
+    console.log(postMessages);
+
+    res.status(200).json(postMessages);
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const createPost = async (req: any, res: any) => {
+  const post = req.body;
+  const newPost = new PostMessage(post);
+  try {
+    await newPost.save();
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(409).json({ message: error.message });
+  }
 };

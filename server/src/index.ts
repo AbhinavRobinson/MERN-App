ac2ad7803b2b35807033667ba79f4b58a6c7386a
Abhinav Robinson <abhinavrobinson@gmail.com>
2021-02-28 20:21:42 +0530

    (feat): use Env variables for key

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,7 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import fs from "fs";
 
 import postRoutes from "../routes/posts";
 
@@ -14,26 +13,19 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // mongoose setup
-fs.readFile(__dirname + "/../../secret.json", "utf8", (err, data) => {
-  if (err) console.log(err);
-  else {
-    const secrets = JSON.parse(data);
+var CONNECTION_URL = process.env.MONGOOSE_URL || "NO API KEY";
+const PORT = process.env.PORT || 5000;
+mongoose
+  .connect(CONNECTION_URL as string, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
 
-    const CONNECTION_URL = secrets.mongoose_url;
-    const PORT = process.env.PORT || 5000;
+mongoose.set("useFindAndModify", false);
 
-    mongoose
-      .connect(CONNECTION_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      })
-      .then(() =>
-        app.listen(PORT, () =>
-          console.log(`Server Running on Port: http://localhost:${PORT}`)
-        )
-      )
-      .catch((error) => console.log(`${error} did not connect`));
-
-    mongoose.set("useFindAndModify", false);
-  }
-});
